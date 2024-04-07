a462a4801cf6747d34e6af75fb2e8675511e201e
agustin <aguss.97@outlook.com>
2024-04-07 13:59:58 -0300

    ordenando los componentes GiftExpertApp y AddCategory, para crear un nuevo componente GiftGrid y asi poder empezar hacer las peticiones a la API

--- 04-gift-expert-app/src/GiftExpertApp.jsx
+++ 04-gift-expert-app/src/GiftExpertApp.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GiftGrid } from "./components/GiftGrid";
 
 //Forma 1
 
@@ -29,27 +30,25 @@ import { AddCategory } from "./components/AddCategory";
 //Forma 2 (correcta de hacer el componente)
 
 export const GiftExpertApp = () => {
-
   const [categories, setCategories] = useState([`One Punch`, `Dragon Ball`]);
 
   //Evitar usar push en los array porque afecta la mutabilidad y eso es algo que en react se tiene que evitar
-  const onAddCategory = ( newCategory ) => {
-    if(categories.includes(newCategory)) return; //Valida que en el array no haya datos repetidos
-    // categories.push(newCategory);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return; //Valida que en el array no haya datos repetidos
     setCategories([...categories, newCategory]);
-    // setCategories( cat => [...cat, `Valorant`] ); 
-  }
+  };
 
   return (
     <>
-        <h1>GiftExpertApp</h1>
-        <AddCategory  
-          onNewCategory={ (value) => onAddCategory(value) }  />  {/*Enviamos el state al componente hijo  */}
-        <ol>
-          { categories.map( category => {
-            return <li key={ category }> { category } </li>
-          } ) }
-        </ol>
+      <h1>GiftExpertApp</h1>
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {categories.map((category) => (
+        <GiftGrid 
+          key={category} 
+          category={category}
+        />
+      ))}
     </>
-  )
-}
+  );
+};
