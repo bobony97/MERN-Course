1e74bfb0927714c651b8f20ec54a0b708ab56343
agustin <aguss.97@outlook.com>
2024-04-07 12:21:34 -0300

    Implementacion de una segunda forma de hacer la comunicacion entre componentes, enviando la informacion al componente padre

--- 04-gift-expert-app/src/GiftExpertApp.jsx
+++ 04-gift-expert-app/src/GiftExpertApp.jsx
@@ -1,20 +1,50 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
 
+//Forma 1
+
+// export const GiftExpertApp = () => {
+
+//   const [categories, setCategories] = useState([`One Punch`, `Dragon Ball`]);
+
+//   //Evitar usar push en los array porque afecta la mutabilidad y eso es algo que en react se tiene que evitar
+//   const onAddCategory = (  ) => {
+//     setCategories([...categories, `Valorant`]);
+//     // setCategories( cat => [...cat, `Valorant`] );
+//   }
+
+//   return (
+//     <>
+//         <h1>GiftExpertApp</h1>
+//         <AddCategory  setCategories={ setCategories } />  {/*Enviamos el state al componente hijo  */}
+//         <ol>
+//           { categories.map( category => {
+//             return <li key={ category }> { category } </li>
+//           } ) }
+//         </ol>
+//     </>
+//   )
+// }
+
+//Forma 2 (correcta de hacer el componente)
+
 export const GiftExpertApp = () => {
 
   const [categories, setCategories] = useState([`One Punch`, `Dragon Ball`]);
 
   //Evitar usar push en los array porque afecta la mutabilidad y eso es algo que en react se tiene que evitar
-  const onAddCategory = (  ) => {
-    setCategories([...categories, `Valorant`]);
-    // setCategories( cat => [...cat, `Valorant`] );  //Otra forma de 
+  const onAddCategory = ( newCategory ) => {
+    console.log(newCategory)
+    // categories.push(newCategory);
+    setCategories([...categories, newCategory]);
+    // setCategories( cat => [...cat, `Valorant`] ); 
   }
 
   return (
     <>
         <h1>GiftExpertApp</h1>
-        <AddCategory  setCategories={ setCategories } />  {/*Enviamos el state al componente hijo  */}
+        <AddCategory  
+          onNewCategory={ (value) => onAddCategory(value) }  />  {/*Enviamos el state al componente hijo  */}
         <ol>
           { categories.map( category => {
             return <li key={ category }> { category } </li>
--- 04-gift-expert-app/src/components/AddCategory.jsx
+++ 04-gift-expert-app/src/components/AddCategory.jsx
@@ -1,6 +1,36 @@
 import { useState } from "react";
 
-export const AddCategory = ({ setCategories }) => { //desestructuramos la prop que recibimos del padre
+//Forma 1
+
+// export const AddCategory = ({ setCategories }) => { //desestructuramos la prop que recibimos del padre
+//   const [inputValue, setInputValue] = useState(``);
+
+//   const onInputChange = (event) => {
+//     setInputValue(event.target.value); //Con el "event.target.value" capturamos el valor ingresado al input
+//   };
+
+//   const onSubmit = (event) => {
+//     event.preventDefault();   //Este evento evita los comportamiento por defecto
+//     if(inputValue.trim().length <= 1) return; //Evita el ingreso de caracteres vacios o de un solo caracter
+//     setCategories( categories => [...categories, inputValue] )  //Y la utilizamos para setear el valor del input al state
+//     setInputValue("");  //Setea un valor vacio luego de ingresar un valor
+//   }
+
+//   return (
+//     <form onSubmit={ onSubmit } >{/*No es necesario pasar el argumento "event" porque el navegador lo hace por si solo al manejador de eventos */}
+//       <input
+//         type="text"
+//         placeholder="Buscar Gift"
+//         value={inputValue} //Setea el valor por defecto del useState
+//         onChange={onInputChange}
+//       />
+//     </form>
+//   );
+// };
+
+//Forma 2 (correcta)
+
+export const AddCategory = ({ onNewCategory }) => { //desestructuramos la prop que recibimos del padre
   const [inputValue, setInputValue] = useState(``);
 
   const onInputChange = (event) => {
@@ -9,8 +39,11 @@ export const AddCategory = ({ setCategories }) => { //desestructuramos la prop q
 
   const onSubmit = (event) => {
     event.preventDefault();   //Este evento evita los comportamiento por defecto
+
     if(inputValue.trim().length <= 1) return; //Evita el ingreso de caracteres vacios o de un solo caracter
-    setCategories( categories => [...categories, inputValue] )  //Y la utilizamos para setear el valor del input al state
+
+    onNewCategory( inputValue.trim() ) //Enviamos el input a la prop
+
     setInputValue("");  //Setea un valor vacio luego de ingresar un valor
   }
 
