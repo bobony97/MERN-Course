7d0984d012796411d01cd33f72a959688027e0d7
agustin <aguss.97@outlook.com>
2024-04-07 11:40:37 -0300

    Comunicacion entre componentes, delegacion de la creacion y agregacion de elementos al array desde el componente hijo

--- 04-gift-expert-app/src/GiftExpertApp.jsx
+++ 04-gift-expert-app/src/GiftExpertApp.jsx
@@ -6,7 +6,7 @@ export const GiftExpertApp = () => {
   const [categories, setCategories] = useState([`One Punch`, `Dragon Ball`]);
 
   //Evitar usar push en los array porque afecta la mutabilidad y eso es algo que en react se tiene que evitar
-  const onAddCategory = () => {
+  const onAddCategory = (  ) => {
     setCategories([...categories, `Valorant`]);
     // setCategories( cat => [...cat, `Valorant`] );  //Otra forma de 
   }
@@ -14,8 +14,7 @@ export const GiftExpertApp = () => {
   return (
     <>
         <h1>GiftExpertApp</h1>
-        <AddCategory />
-        <button onClick={ onAddCategory } >Agregar</button>
+        <AddCategory  setCategories={ setCategories } />  {/*Enviamos el state al componente hijo  */}
         <ol>
           { categories.map( category => {
             return <li key={ category }> { category } </li>
--- 04-gift-expert-app/src/components/AddCategory.jsx
+++ 04-gift-expert-app/src/components/AddCategory.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export const AddCategory = () => {
-  const [inputValue, setInputValue] = useState(`OnePunch`);
+export const AddCategory = ({ setCategories }) => { //desestructuramos la prop que recibimos del padre
+  const [inputValue, setInputValue] = useState(``);
 
   const onInputChange = (event) => {
     setInputValue(event.target.value); //Con el "event.target.value" capturamos el valor ingresado al input
@@ -9,10 +9,13 @@ export const AddCategory = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();   //Este evento evita los comportamiento por defecto
+    if(inputValue.trim().length <= 1) return; //Evita el ingreso de caracteres vacios o de un solo caracter
+    setCategories( categories => [...categories, inputValue] )  //Y la utilizamos para setear el valor del input al state
+    setInputValue("");  //Setea un valor vacio luego de ingresar un valor
   }
 
   return (
-    <form onSubmit={ (event) => onSubmit(event) } >
+    <form onSubmit={ onSubmit } >{/*No es necesario pasar el argumento "event" porque el navegador lo hace por si solo al manejador de eventos */}
       <input
         type="text"
         placeholder="Buscar Gift"
