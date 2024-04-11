a9e7bf216ec6de688d132517fdb676ff82e2c9af
agustin <aguss.97@outlook.com>
2024-04-10 23:13:05 -0300

    implementacion del custom hook useCounter al custom hook useFetch para traer la informacion de la API

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,17 +1,45 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
+import { LoadingMessage } from "./LoadingMessage";
+import { PokemonCard } from "./PokemonCard";
 
 export const MultipleCustomHooks = () => {
 
-    const { data, loading, hasError } = useFetch( `https://pokeapi.co/api/v2/pokemon/3` );
+    const { counter, increment, decrement } = useCounter(1);
+    const { data, loading, hasError } = useFetch( `https://pokeapi.co/api/v2/pokemon/${counter}` );
 
   return (
     <>
         <h1> Información de Pokemon </h1>
         <hr />
         {
-            loading && <p>Cargando...</p> 
+            loading 
+            ? <LoadingMessage /> 
+            :( 
+               <PokemonCard 
+               id={data.id} 
+               name={data.name}
+               sprites={ [
+                data.sprites.front_default,
+                data.sprites.front_shiny,
+                data.sprites.back_default,
+                data.sprites.back_shiny
+               ] }
+               /> 
+            )
         }
-        <h2> { data?.name } </h2>
+
+        <button
+        className="btn btn-primary mt-2"
+        onClick={() => counter > 1 ? decrement() : null}>
+            Anterior
+        </button>
+
+        <button
+        className="btn btn-primary mt-2"
+        onClick={() => increment()}>
+            Siguiente
+        </button>
     </>
   )
 }
--- 05-hook-app/src/hooks/useCounter.js
+++ 05-hook-app/src/hooks/useCounter.js
@@ -7,7 +7,7 @@ export const useCounter = ( initialValue = 10 ) => {  //La persona envia el valo
     const [counter, setCounter] = useState(initialValue);
 
     //Funcion de retorno para nuestro custom hook, que incrementa el estado en +1
-    const increment = (value) => {     //Si queremos que el numero no se incremente de 1 en 1, podemos enviarle un parametro para que el usuario determine de cuanto se va a incrementar
+    const increment = (value = 1) => {     //Si queremos que el numero no se incremente de 1 en 1, podemos enviarle un parametro para que el usuario determine de cuanto se va a incrementar
         setCounter(counter + value);
     }
 
