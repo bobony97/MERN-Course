2c81d3eee7ffd8159a95662ddb1031c135570354
agustin <aguss.97@outlook.com>
2024-04-28 02:08:12 -0300

    se agrega pantalla de imagenes, pantalla de notas y boton para agregar nuevas notas y componente

--- 07-journal-app/src/journal/pages/JournalPage.jsx
+++ 07-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,10 +1,28 @@
-import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../../views/NothingSelectedView'
+import { NoteView } from '../../views/NoteView'
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores rerum nam odit quae ullam magni voluptas fugit. Laudantium quos incidunt repellendus quaerat, tenetur consequuntur ducimus, perspiciatis doloribus fuga dolorem sed.</Typography>
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      
+      <IconButton
+        size='large'
+        sx={{ 
+          color: 'white',
+          backgroundColor: 'error.main',
+          'hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+      }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
