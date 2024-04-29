708c9702225c0b94b330645b597c145b4f481d10
agustin <aguss.97@outlook.com>
2024-04-29 10:28:45 -0300

    se creo el thunk, se hizo el llamado a la api de pokemons y se los renderizo en el componente PokemonApp

--- 08-toolkit-redux/src/PokemonApp.jsx
+++ 08-toolkit-redux/src/PokemonApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon/thunks';
 
 
 export const PokemonApp = () => {
 
+  const { isLoading, pokemons = [], page } = useSelector(state => state.pokemons)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,10 +16,22 @@ export const PokemonApp = () => {
     <>
         <h1>PokemonApp</h1>
         <hr />
+        <span>Loading: { isLoading ? 'true' : 'false' } </span>
 
         <ul>
-            <li>Hola</li>
+            {
+              pokemons.map(pokemon => (
+                <li key={pokemon.name}> {pokemon.name} </li>
+              ))
+            }
         </ul>
+
+        <button 
+          disabled={isLoading}
+          onClick={() => dispatch(getPokemons(page))}
+        >
+          Next
+        </button>
     </>
   )
 }
--- 08-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 08-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -8,13 +8,18 @@ import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
     En Redux Toolkit y en el ecosistema de Redux en general, los "thunks" son una forma de interactuar con la capa asíncrona de una aplicación Redux, 
     como las llamadas a API, la gestión de efectos secundarios, las operaciones de carga de datos, entre otras tareas complejas que no se pueden manejar directamente en un reducer síncrono.
 */
+
+//Los parámetros "dispatch" y "getState" que recibe la función asíncrona son proporcionados por Redux Toolkit automáticamente cuando se utiliza createAsyncThunk para crear el thunk.
+/*
+    Cuando defines un thunk con createAsyncThunk, no necesitas pasar explícitamente dispatch o getState como argumentos al invocar el thunk.
+    Redux Toolkit se encarga de inyectar estos argumentos automáticamente cuando se llama al thunk, de manera que puedas utilizarlos dentro de la función asíncrona para despachar 
+    acciones y obtener el estado actual.
+*/
 export const getPokemons = ( page = 0 ) => {
     return  async( dispatch, getState ) => {
         dispatch( startLoadingPokemons() );
 
-        //TODO: Realizar peticion http
         const { data } = await pokemonApi.get(`pokemon?limit=10&offset=${ page * 10 }`);
-        console.log(resp)
 
         dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
     }
