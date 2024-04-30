56936e4e41755193743266811317ed63382e6ea0
agustin <aguss.97@outlook.com>
2024-04-30 20:08:50 -0300

    se creo un componente para el checking authentication y se implemento la logica para mantener el estado del login al recargar la pagina

--- 07-journal-app/src/router/AppRouter.jsx
+++ 07-journal-app/src/router/AppRouter.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { AuthRoutes } from '../auth/routes/AuthRoutes'
-import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { useDispatch, useSelector } from "react-redux";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth/authSlice";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged( FirebaseAuth, async(user) => {
+      if(!user) return dispatch( logout() );
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    } )
+
+  }, []);
+
+  if(status === 'checking') return <CheckingAuth />
+
   return (
     <Routes>
-        {/*Login y Registro */}
-        <Route path='/auth/*' element={ <AuthRoutes /> } />
+      {
+        (status === 'authenticated')
+        ? <Route path="/*" element={<JournalRoutes />} /> : <Route path="/auth/*" element={<AuthRoutes />} />
+      }
 
-        {/*JournalApp */}
-        <Route path='/*' element={ <JournalRoutes /> } />
+      <Route path="/*" element={ <Navigate to='/auth/login' /> } />
     </Routes>
-  )
-}
+  );
+};
