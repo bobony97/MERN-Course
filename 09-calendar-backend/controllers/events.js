942b725409990b85f549619f72cede7e528da308
agustin <aguss.97@outlook.com>
2024-05-11 20:01:19 -0300

    se implemento la actualizacion de eventos, con validacion de usuario a la hora de actualizar dichos eventos

--- 09-calendar-backend/controllers/events.js
+++ 09-calendar-backend/controllers/events.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const Evento = require('../models/Event');
+const Event = require('../models/Event');
 
 
 const getEvents = async(req, res = response) => {
@@ -9,18 +9,18 @@ const getEvents = async(req, res = response) => {
         Esto significa que populate() permite sustituir el ID de referencia en un documento por el documento real al que hace referencia ese ID en otra colección.
         La propiedad "name", extrae dicha propiedad del objeto y lo muestra en lugar del resto de las propiedades
     */
-    const eventos = await Evento.find()
+    const events = await Event.find()
                                 .populate('user', 'name');
 
     return res.status(201).json({
         ok: true,
-        eventos
+        events
     })
 }
 
 const createEvent = async(req, res = response) => {
 
-    const event = new Evento( req.body );
+    const event = new Event( req.body );
 
     try {
         evento.user = req.uid; //Le asignamos a la propiedad "user" de la colección "Event" el id del usuario de la colección "User" para establecer la relación
@@ -40,13 +40,49 @@ const createEvent = async(req, res = response) => {
     }
 }
 
-const updateEvent = (req, res = response) => {
+const updateEvent = async(req, res = response) => {
 
+    const eventId = req.params.id; //Accede al id enviado por la url
+    const uid = req.uid; //Extrae el uid del usuario que creo el evento
 
-    return res.status(201).json({
-        ok: true,
-        msg: 'Actualizando Evento'
-    })
+    try {
+        const event = await Event.findById( eventId ); //Trae un evento de la DB por el id 
+
+        if( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El evento con ese id no existe'
+            });
+        }
+
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para editar este evento'
+            })
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        //findByIdAndUpdate Espera 2 parámetros, el primero es el id del evento que quiere actualizar y el segundo es el evento actualizado
+        //Esto va a regresar el evento antiguo pero si va a actualizar el evento nuevo en la DB, si queremos que regrese el evento actualizado se debe enviar un tercer parametro "{ new: true }""
+        const newEventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+        return res.status(201).json({
+            ok: true,
+            newEventUpdated
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        })
+    }
 }
 const deleteEvent = (req, res = response) => {
 
--- 09-calendar-backend/routes/events.js
+++ 09-calendar-backend/routes/events.js
@@ -22,7 +22,16 @@ router.post(
 
 router.get('/', getEvents);
 
-router.put('/:id', updateEvent);
+router.put(
+    '/:id',
+    [
+        check('title', 'El titulo es obligatorio').notEmpty(),
+        check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'La fecha de finalización es obligatoria').custom(isDate),
+        fieldsValidator
+    ],
+    updateEvent
+);
 
 router.delete('/:id',  deleteEvent);
 
