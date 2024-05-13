c89b8682214632a73fc5913e81ff113ebb4d9543
agustin <aguss.97@outlook.com>
2024-05-13 20:06:35 -0300

    se implemento el manejo de los datos y errores a la hora de hacer el login

--- 09-calendar/src/hooks/useAuthStore.js
+++ 09-calendar/src/hooks/useAuthStore.js
@@ -1,18 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import calendarAPI from '../api/calendarAPI.JS';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store/auth/authSlice';
 
 export const useAuthStore = () => {
     const { status, user, errorMessage } = useSelector(state => state.auth);
     const dispatch = useDispatch()
 
     const startLogin = async({ email, password }) => {
-        console.log({email, password})
+        dispatch(onChecking());
 
         try {
-            const resp = await calendarAPI.post('/auth', {email, password});
-            console.log({resp})
+            const {data} = await calendarAPI.post('/auth', {email, password});
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
+
         } catch (error) {
-            console.log(error)
+            dispatch(onLogout('Credenciales Incorrectas'));
+            setTimeout(() => {
+                dispatch(clearErrorMessage());
+            }, 10)
         }
     }
 
