96394413c46733f0e6e63026d2dbbd863bfce0a9
agustin <aguss.97@outlook.com>
2024-05-13 19:47:03 -0300

    se implemento la conexion con el backend para el login

--- 09-calendar/src/auth/pages/LoginPage.jsx
+++ 09-calendar/src/auth/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useAuthStore } from '../../hooks/useAuthStore';
 import { useForm } from '../../hooks/useForm';
 import './loginPage.css';
 
@@ -15,12 +16,14 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
+    const { startLogin } = useAuthStore()
+
     const { loginEmail, loginPassword, onInputChange:onLoginInputChange } = useForm(loginFormFields);
     const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange:onInputRegisterChange } = useForm(registerFormFields);
 
     const loginSubmit = (event) => {
         event.preventDefault();
-        console.log({loginEmail, loginPassword})
+        startLogin({email:loginEmail, password:loginPassword});
     }
 
     const registerSubmit = (event) => {
